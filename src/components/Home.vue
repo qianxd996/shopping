3dbcbead1d66031ccdafc582c975a2d7475f5b4b
qianxd996 <qianxd996@163.com>
2020-10-23 19:58:47 +0800

    login finish

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <el-button type="info" plain @click="logout">系统退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+    // 销毁本地的token
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -12,8 +13,21 @@ const router = new VueRouter({
     }, {
       path: '/login',
       component: Login
+    }, {
+      path: '/home',
+      component: Home
     }
   ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // 用户直接访问登录页 直接放行
+  if (to.path === '/login') return next()
+  // 判断是否登录
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login') // 没有登录 强制跳转导登录页
+  next()
 })
 
 export default router
